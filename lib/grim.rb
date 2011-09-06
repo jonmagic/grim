ecab1ed3dbec01b528d89fe565e8678b02c5dff6
Jonathan Hoyt <jonmagic@gmail.com>
2011-09-05 21:13:45 -0400

    Trying to adhere to TomDoc and also un-nested specs since we don't have a Pdf class anymore. Also added license.

--- lib/grim.rb
+++ lib/grim.rb
@@ -5,8 +5,8 @@
 #
 #    instance    = Grim.new("/path/to/pdf")
 #    page_count  = instance.page_count
-#    png         = instance.page(1).to_png("/path/to/save/png")
-#    jpeg        = instance.page(2).to_jpeg("/path/to/save/jpeg")
+#    png         = instance.page(1).to_image("/path/to/save/image.png")
+#    jpeg        = instance.page(2).to_image("/path/to/save/image.jpeg")
 #    text        = instance.page(3).text
 #
 class Grim
@@ -34,34 +34,37 @@ class Grim
   attr_accessor :page_number
 
   # initialize is called when a new instance is created and accepts path.
+  #
+  # path - A String or Path to the pdf
   def initialize(path)
     raise Grim::PdfNotFound unless File.exists?(path)
     @page_number = 1
     @path = path
   end
 
-  # page_count uses the memoized path and shells out to ghostscript
-  # to read the pdf with the pdf_info.ps script as a filter,
-  # returning the number of pages in the pdf as an integer.
+  # Shells out to ghostscript to read the pdf with the pdf_info.ps script
+  # as a filter, returning the number of pages in the pdf as an integer.
   #
   # For example:
   #
-  #    instance.page_count
-  # => 4
+  #   instance.page_count
+  #   # => 4
   #
-  # Returns an integer.
+  # Returns an Integer.
   def page_count
     @page_count ||= begin
       `gs -dNODISPLAY -q -sFile=#{@path} ./lib/pdf_info.ps`.to_i
     end
   end
 
-  # page just sets the page attribute on the instance.
+  # Sets the page attribute on the instance.
+  #
+  # number - An Integer of the page number to extract
   #
   # For example:
   #
-  #    instance.page(1)
-  # => instance
+  #   instance.page(1)
+  #   # => self
   #
   # Returns self.
   def page(number)
@@ -69,20 +72,30 @@ class Grim
     self
   end
 
-  # Returns page_number minus 1
+  # Page number minus 1
+  #
+  # For example:
+  #
+  #   instance.page(2)
+  #   instance.index
+  #   # => 1
+  #
+  # Returns an Integer.
   def index
     @page_number - 1
   end
 
-  # to_image extracts the selected page and turns it into an image.
+  # Extracts the selected page and turns it into an image.
   # Tested on png and jpeg.
+  #
+  # path - A String or Path to save image to
   #
   # For example:
   #
-  #    instance.page(2).to_image(/path/to/save/image)
-  # => File
+  #   instance.page(2).to_image(/path/to/save/image)
+  #   # => File
   #
-  # Returns an instance of File
+  # Returns a File.
   def to_image(path)
     `convert -resize #{Grim::WIDTH} -antialias -render -quality #{Grim::QUALITY} -colorspace RGB -interlace none -density #{Grim::DENSITY} #{@path}[#{index}] #{path}`
     file = File.open(path)
@@ -90,14 +103,14 @@ class Grim
     file
   end
 
-  # text is an instance method that extracts the text from the selected page.
+  # Extracts the text from the selected page.
   #
   # For example:
   #
-  #    instance.page(2).text
-  # => "This is text from slide 2.\n\nAnd even more text from slide 2."
+  #   instance.page(2).text
+  #   # => "This is text from slide 2.\n\nAnd even more text from slide 2."
   #
-  # Returns a string
+  # Returns a String.
   def text
     `pdftotext -enc UTF-8 -f #{@page_number} -l #{@page_number} #{@path} -`
   end
--- spec/lib/grim_spec.rb
+++ spec/lib/grim_spec.rb
@@ -10,86 +10,84 @@ describe Grim do
     Grim.const_defined?('VERSION').should be_true
   end
 
-  describe "Pdf" do
-    describe "#initialize" do
-      it "should raise an error if pdf does not exist" do
-        lambda { Grim.new(fixture_path("booboo.pdf")) }.should raise_error(Grim::PdfNotFound)
+  describe "#initialize" do
+    it "should raise an error if pdf does not exist" do
+      lambda { Grim.new(fixture_path("booboo.pdf")) }.should raise_error(Grim::PdfNotFound)
+    end
+  end
+
+  describe "#page_count" do
+    it "should return an integer" do
+      instance = Grim.new(fixture_path("smoker.pdf"))
+      instance.page_count.should == 25
+    end
+  end
+
+  describe "#page" do
+    it "should be set to 1 by default" do
+      instance = Grim.new(fixture_path("smoker.pdf"))
+      instance.page_number.should == 1
+    end
+
+    it "should set page attribute and return instance" do
+      instance = Grim.new(fixture_path("smoker.pdf"))
+      instance.page(2).should == instance
+      instance.page_number.should == 2
+    end
+  end
+
+  describe "#index" do
+    it "should return page minus 1" do
+      instance = Grim.new(fixture_path("smoker.pdf"))
+      instance.page(2)
+      instance.index.should == 1
+    end
+  end
+
+  describe "#to_image" do
+    describe "output png" do
+      before(:all) do
+        instance = Grim.new(fixture_path("smoker.pdf"))
+        @png = instance.to_image(tmp_path("to_png_spec.png"))
+      end
+
+      it "should create the file" do
+        File.exist?(tmp_path("to_png_spec.png")).should be_true
+      end
+
+      it "should return an instance of File" do
+        @png.class.should == File
+      end
+
+      it "should have the right file size" do
+        @png.stat.size.should == 188515
       end
     end
 
-    describe "#page_count" do
-      it "should return an integer" do
+    describe "output jpeg" do
+      before(:all) do
         instance = Grim.new(fixture_path("smoker.pdf"))
-        instance.page_count.should == 25
+        @jpeg = instance.to_image(tmp_path("to_jpeg_spec.jpeg"))
+      end
+
+      it "should create the file" do
+        File.exist?(tmp_path("to_jpeg_spec.jpeg")).should be_true
+      end
+
+      it "should return an instance of File" do
+        @jpeg.class.should == File
+      end
+
+      it "should have the right file size" do
+        @jpeg.stat.size.should == 53980
       end
     end
+  end
 
-    describe "#page" do
-      it "should be set to 1 by default" do
-        instance = Grim.new(fixture_path("smoker.pdf"))
-        instance.page_number.should == 1
-      end
-
-      it "should set page attribute and return instance" do
-        instance = Grim.new(fixture_path("smoker.pdf"))
-        instance.page(2).should == instance
-        instance.page_number.should == 2
-      end
-    end
-
-    describe "#index" do
-      it "should return page minus 1" do
-        instance = Grim.new(fixture_path("smoker.pdf"))
-        instance.page(2)
-        instance.index.should == 1
-      end
-    end
-
-    describe "#to_image" do
-      describe "output png" do
-        before(:all) do
-          instance = Grim.new(fixture_path("smoker.pdf"))
-          @png = instance.to_image(tmp_path("to_png_spec.png"))
-        end
-
-        it "should create the file" do
-          File.exist?(tmp_path("to_png_spec.png")).should be_true
-        end
-
-        it "should return an instance of File" do
-          @png.class.should == File
-        end
-
-        it "should have the right file size" do
-          @png.stat.size.should == 188515
-        end
-      end
-
-      describe "output jpeg" do
-        before(:all) do
-          instance = Grim.new(fixture_path("smoker.pdf"))
-          @jpeg = instance.to_image(tmp_path("to_jpeg_spec.jpeg"))
-        end
-
-        it "should create the file" do
-          File.exist?(tmp_path("to_jpeg_spec.jpeg")).should be_true
-        end
-
-        it "should return an instance of File" do
-          @jpeg.class.should == File
-        end
-
-        it "should have the right file size" do
-          @jpeg.stat.size.should == 53980
-        end
-      end
-    end
-
-    describe "#text" do
-      it "should return the text from the selected page" do
-        instance = Grim.new(fixture_path("smoker.pdf"))
-        instance.page(2).text.should == "Step 1: get someone to print this curve for you to scale, 72\342\200\235 wide\n\nStep 2: Get a couple 55 gallon drums\n\n\f"
-      end
+  describe "#text" do
+    it "should return the text from the selected page" do
+      instance = Grim.new(fixture_path("smoker.pdf"))
+      instance.page(2).text.should == "Step 1: get someone to print this curve for you to scale, 72\342\200\235 wide\n\nStep 2: Get a couple 55 gallon drums\n\n\f"
     end
   end
 end
